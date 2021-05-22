c4c4d6887b1fe2962ffa0862c25f7c5a2fb8c907
ghdl023 <gwee1216@naver.com>
2021-05-22 14:48:38 +0900

    하단 네비게이터

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_app/widget/footer.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Netflix',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white,
+        ),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: <Widget>[
+                  Container(
+                    child: Center(
+                      child: Text('홈'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('검색'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('저장한 컨텐츠'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('더보기'),
+                    ),
+                  ),
+                ],
+              ),
+              bottomNavigationBar: Footer(),
+            )));
+  }
 }
